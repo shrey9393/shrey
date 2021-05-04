c1af3ef65cfee90a589e9bc84e627d60225c7495
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2021-05-05 00:00:03 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,45 @@
+import os
+print("hello world")
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+import os
+print(os.listdir())
+from playsound import playsound
+playsound('D:\\sem-1 computere science\\Python Course with Notes\\Chapter 1 - Modules, Comments & Pip\\play.mp3')
+a="shreyansh"
+b=345
+c=45.32
+d=True
+e=None
+#printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+#to print the type of veriable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+a=int(input("insert first number:- "))
+b=int(input("insert second number:- "))
+print("value of a+b is", a+b)
+print("value of a-b is", a-b)
+print("value of a*b is", a*b)
+print("value of a/b is", a/b)
+print("value of a//b is", a//b)
+# strings
+x=74
+y="shreyansh"
+print(x,y)
+print(type(x))
+print(type(y))
+z=" agrawal"
+h=y+z
+print(h)
+print(y[0])
+print(len(h))
+print(h[0:10])
+print(h[-4:-1])
